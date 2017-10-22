8f1d9f4006836d5bad41f3b5b1b3510c70a95618
Marco Langes <marcolanges@gmail.com>
2017-10-22 16:32:08 +0200

    File structure
    And setup main window

--- Centrale/main.py
+++ Centrale/main.py
@@ -1,37 +1,17 @@
-import os
-import serial
-import threading
-import time
+#Imports
+from classes.arduino import arduino
+import tkinter
 
-#class for communicating with the arduino
-# http://www.instructables.com/id/Arduino-Python-Communication-via-USB/
-# https://www.python-course.eu/threads.php
-class Arduino(threading.Thread):
-	def __init__(self, comport, baudrate):
-		threading.Thread.__init__(self)
-		self.com = None
-		self.counter = 0
-		#self.com = serial.Serial(comport , baudrate, timeout=.1)
-		self.running = True
-	def run(self):
-		while self.running:
-			data = None
-			if self.com != None:
-				data = self.com.readline()
+#Create window
+#Title, size, etc.
+window = tkinter.Tk()
+window.title("Arduino app")
+window.geometry("800x600")
+window.wm_iconbitmap('images/arduino_icon.ico')
 
-			self.counter += 1
-			if self.counter > 3:
-				self.running = False
 
-			print("Counter is " + str(self.counter))
-			time.sleep(1)
 
-running = True
-com = Arduino('COM1', 96000)
-com.start()
+#Start window
+window.mainloop()
 
-while running:
-	#Run the program
-	time.sleep(1)
-
-com.join()
+#test = Arduino(1,1)
