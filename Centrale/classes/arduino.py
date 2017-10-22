fea1fd8f6cde563acd897e39d2a805c00ab01bfe
Joshua <joshuahelfferich@hotmail.com>
2017-10-22 16:47:56 +0200

    Cleanup arduino class

--- Centrale/classes/arduino.py
+++ Centrale/classes/arduino.py
@@ -6,12 +6,13 @@ import time
 #class for communicating with the arduino
 # http://www.instructables.com/id/Arduino-Python-Communication-via-USB/
 # https://www.python-course.eu/threads.php
-class Arduino(threading.Thread):
+class arduino(threading.Thread):
 	def __init__(self, comport, baudrate):
 		threading.Thread.__init__(self)
+		# Init the comport to communicate with the arduino
+		#self.com = serial.Serial(comport , baudrate, timeout=.1)
 		self.com = None
 		self.counter = 0
-		#self.com = serial.Serial(comport , baudrate, timeout=.1)
 		self.running = True
 	def run(self):
 		while self.running:
@@ -25,15 +26,3 @@ class Arduino(threading.Thread):
 
 			print("Counter is " + str(self.counter))
 			time.sleep(1)
-
-
-
-#running = True
-#com = Arduino('COM1', 96000)
-#com.start()
-
-#while running:
-	#Run the program
-	#time.sleep(1)
-
-#com.join()
