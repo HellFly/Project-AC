8079ccffaef0013b0beff694a5195e675e674a52
Joshua <joshuahelfferich@hotmail.com>
2017-10-26 13:42:29 +0200

    Added functions to send measured data

--- Besturingseenheid/SerialTest/SerialTest.ino
+++ Besturingseenheid/SerialTest/SerialTest.ino
@@ -3,7 +3,7 @@ byte sendBuffer[5];
 
 void setup() {
   Serial.begin(9600);
-  
+
   // reserve 16 bytes for the input data:
   inputBuffer.reserve(16);
 }
@@ -25,7 +25,7 @@ void serialEvent() {
   while (Serial.available()) {
     inputBuffer += (char)Serial.read();
   }
-  
+
   parseData();
 }
 
@@ -55,5 +55,66 @@ void parseData() {
   else { // no valid value for the packet id, so clear the data
     inputBuffer = "";
   }
+}
+
+void send_light(int light) {
+ byte val1;
+  byte val2;
+
+  if (light < 0) {
+    val1 = 0;
+    val2 = 0;
+  }
+  else if (light > 255*256+255) { // if light value > max value able to send
+    val1 = 255;
+    val2 = 255;
+  }
+  else {
+    val1 = (byte)light / 256;
+    val2 = (byte)light % 256;
+  }
+
+
+  byte buffer[3];
+  buffer[0] = 1;
+  buffer[1] = val1;
+  buffer[2] = val2;
+  Serial.write(buffer, 3);
+}
+
+void send_temperature(int temp) {
+	temp += 128; // offset of 128 degrees
+	byte val;
+
+	if (temp < 0) {
+		val = 0;
+	}
+	else if (temp > 255) {
+		val = 255;
+	}
+	else {
+		val = (byte)temp;
+	}
+
+	byte buffer[2];
+	buffer[0] = 2;
+	buffer[1] = val;
+	Serial.write(buffer, 2);
+}
+
+void send_shutter_lightunit(bool is_open) {
+  byte buffer[3];
+  buffer[0] = 3;
+  buffer[1] = 0;
+  buffer[2] = (byte)is_open;
+  Serial.write(buffer, 3);
+}
+
+void send_shutter_tempunit(bool is_open) {
+  byte buffer[3];
+  buffer[0] = 3;
+  buffer[1] = 1;
+  buffer[2] = (byte)is_open;
+  Serial.write(buffer, 3);
 }
 
